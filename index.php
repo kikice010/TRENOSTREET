5c2b6c24bf28f6516bc27351d953f2b356f68787
kikice010 <kikice010@gmail.com>
2017-02-11 11:58:31 +0100

    folder structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html lang="en" class="no-js oldie ie6"> <![endif]-->
+<!--[if IE 7 ]> <html lang="en" class="no-js oldie ie7"> <![endif]-->
+<!--[if IE 8 ]> <html lang="en" class="no-js oldie ie8"> <![endif]-->
+<!--[if (gt IE 8)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head></head>
+<body>
+HELLO!
+</body>
+</html>
