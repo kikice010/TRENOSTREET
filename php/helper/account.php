12976e49e5cf04299e17296c48fe1c7cefaa4c29
JONNY\jonnychuck <alberto.villanueva.av@gmail.com>
2017-03-12 13:26:05 +0100

    Account helper

---
+++ php/helper/account.php
@@ -0,0 +1,23 @@
+<?php
+
+require "../conx/conx.php";
+$connection = new DB();
+
+class Account {
+
+    private static function createNewUserAccount() {
+        
+    }
+
+    private static function createNewTrainerAccount() {
+        
+    }
+
+    private static function removeAccount($id_course) {
+        
+    }
+   
+
+}
+
+?>
