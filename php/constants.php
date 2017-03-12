30dd79b1013c8c10773246d98b920beae6af09bd
JONNY\jonnychuck <alberto.villanueva.av@gmail.com>
2017-03-12 11:47:33 +0100

    Course Controller

--- php/constants.php
+++ php/constants.php
@@ -1,23 +1,31 @@
 <?php
 
-class TextConstants
-{
-const COPYRIGHT ="&copy; 2017 TrenoStreet. All Rights Reserved.";
-  const TRENOSTREET = "TrenoStreet";
-  const DESCRIPTION = "Ahora puedes entrenar con los mejores entrenadores de tu ciudad!";
-  const AUTHOR = "TrenoStreet Team";
-  const KEYWORDS ="";
+class TextConstants {
+
+    const COPYRIGHT = "&copy; 2017 TrenoStreet. All Rights Reserved.";
+    const TRENOSTREET = "TrenoStreet";
+    const DESCRIPTION = "Ahora puedes entrenar con los mejores entrenadores de tu ciudad!";
+    const AUTHOR = "TrenoStreet Team";
+    const KEYWORDS = "";
 
 }
 
+class SocialConstants {
 
-class SocialConstants{
-
-const FACEBOOK_URL ="";
-const INSTAGRAM_URL ="";
-const TWITTER_URL="";
+    const FACEBOOK_URL = "";
+    const INSTAGRAM_URL = "";
+    const TWITTER_URL = "";
 
 }
 
+class CourseRequestType {
+
+    const SINGLE_COURSE_GET = 0;
+    const MULTIPLE_COURSE_GET = 1;
+    const SINGLE_COURSE_GET = 0;
+    const SINGLE_COURSE_GET = 0;
+    const SINGLE_COURSE_GET = 0;
+
+}
 
 ?>
