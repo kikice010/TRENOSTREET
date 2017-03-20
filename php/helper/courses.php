aee0c4cfa33bfcf54921de69a1874fc6e3ae23c2
JONNY\jonnychuck <alberto.villanueva.av@gmail.com>
2017-03-20 23:29:35 +0100

    Add data

--- php/helper/courses.php
+++ php/helper/courses.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 class COURSES {
 
     private $db;
@@ -10,12 +12,13 @@ class COURSES {
 
     public function createNewCourse($course_name, $course_category, $course_city, $course_address, $course_description, $course_yearly, $course_montly, $course_weekly, $course_hourly) {
         try {
-       
+
             $stmt = $this->conn->db->prepare("INSERT INTO courses (`id_city`, `id_trainer`, `description`, `price_monthly`, `price_weekly`, `price_yearly`, `price_hour`, `name`, `address`)"
-                    . " VALUES (course_name, '3', 'sss', '1', '2', '3', '4', 'wwww', 'ecececec');");
-            
+                    . " VALUES (:course_city,:trainer,:course_description,:course_montly,:course_weekly,:course_yearly,:course_hourly,:course_name,:course_address);");
+
             $stmt->bindparam(":course_name", $course_name);
             $stmt->bindparam(":course_category", $course_category);
+            $stmt->bindparam(":trainer", $_SESSION['user_session']);
             $stmt->bindparam(":course_city", $course_city);
             $stmt->bindparam(":course_address", $course_address);
             $stmt->bindparam(":course_description", $course_description);
@@ -23,7 +26,7 @@ class COURSES {
             $stmt->bindparam(":course_montly", $course_montly);
             $stmt->bindparam(":course_weekly", $course_weekly);
             $stmt->bindparam(":course_hourly", $course_hourly);
-      
+
             $stmt->execute();
             return $stmt;
         } catch (PDOException $e) {
